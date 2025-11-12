8d76cbd832e622dea2ed6243777a6695920e5d6e
lisegmb <lise.gamboa@u-bordeaux.fr>
2025-11-12 14:29:59 +0100

    Initial commit

---
+++ R/mvnpdf.R
@@ -0,0 +1,39 @@
+#' mvnpdf
+#'this function calculate the (log)normal distribution for a matrix
+#' @param x the matrix of the observations
+#' @param mean a vector, the mean for each variable
+#' @param varcovM the matrix of variance, covariance
+#' @param Log logical, default equal TRUE if TRUE calculate the log normal distribution
+#'
+#' @returns the observation and the prediction with the (log)normal distribution
+#' @export
+#' @import mvtnorm
+#' @examples
+#' mvnpdf(x=matrix(1.96), Log=FALSE)
+
+
+
+
+mvnpdf <- function(x, mean =  rep(0, nrow(x)),
+                   varcovM = diag(nrow(x)), Log = TRUE) {
+  n <- ncol(x)
+  p <- nrow(x)
+  x0 <- x - mean
+  Rinv <- solve(varcovM)
+  LogDetvarcovM <- log(det(varcovM))
+
+  y <- NULL
+  for (j in 1:n) {
+    yj <- - p/2 * log(2*pi) - 0.5 * LogDetvarcovM -
+      0.5 * t(x0[, j]) %*% Rinv %*% x0[, j]
+    y <- c(y, yj)
+  }
+
+  if (!Log) {
+    y <- exp(y)
+  }
+
+  res <- list(x = x, y = y)
+  class(res) <- "mvnpdf"
+  return(res)
+}
